379c7fbf77049eb1297fbb02e51502676169eb28
Dudu7352 <aeiou7352@gmail.com>
2023-10-26 22:42:01 +0200

    Update: create basic generate_image method

--- src/frac_generator.rs
+++ src/frac_generator.rs
@@ -1,3 +1,7 @@
+use image::ImageBuffer;
+
+use std::thread;
+
 use crate::options::fractal_options::FractalOptions;
 
 pub struct FracGenerator {
@@ -9,5 +13,31 @@ impl FracGenerator {
         Self { 
             opts
         } 
+    }
+
+    pub async fn generate_image(&self, threads: usize) -> ImageBuffer<image::Luma<u8>, Vec<u8>> {
+        let mut pixels = Vec::with_capacity(self.opts.resolution * self.opts.resolution);
+        let mut th = Vec::with_capacity(threads);
+
+        for i in 0..threads {
+            
+            let end = if i == threads - 1 {
+                self.opts.resolution
+            } else {
+                (self.opts.resolution / threads) * i + 1
+            };
+
+            let opts_clone = self.opts.clone();
+            th.push(thread::spawn(move || {
+                //TODO: image part calculation
+                vec![]
+            }));
+        }
+
+        for t in th {
+            pixels.append(&mut t.join().unwrap());
+        }
+
+        ImageBuffer::from_raw(self.opts.resolution as u32, self.opts.resolution as u32, pixels).unwrap()
     }
 }
