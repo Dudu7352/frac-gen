135a73aa068135174d83b1240d849661bcb6455d
Dudu7352 <aeiou7352@gmail.com>
2023-10-27 21:55:57 +0200

    Update: add len() method to RenderRange

--- src/options/render_range.rs
+++ src/options/render_range.rs
@@ -7,4 +7,8 @@ impl RenderRange {
     pub fn new(start: usize, end: usize) -> Self {
         Self { start, end }
     }
+
+    pub fn len(&self) -> usize {
+        self.end - self.start
+    }
 }
